b7c449cc5ffe28a449d2c4b263cf6bec0b7828fc
Nord.A <mr.nordakov@gmail.com>
2023-07-21 15:09:14 +0300

    seminar0

---
+++ seminars/seminar0/script.js
@@ -0,0 +1,57 @@
+// Задание 1
+
+console.log("Текст из script.js");
+
+// Задание 2
+
+let num = 123;
+console.log(num);
+
+let a = 5;
+a = 7;
+console.log(a);
+
+let b = 1 + 2 + 3;
+console.log(b);
+
+// Изи бризи
+
+// Задание 3
+
+const c = 10;
+const d = 2;
+
+let sum = c + d;
+let difference = c - d;
+let multiplication = c * d;
+let division = c / d;
+
+console.log(c, d, sum, difference, multiplication, division);
+console.log(c + d + sum + difference + multiplication + division);
+
+// Задание 5
+
+const q = 1.5;
+const w = 0.75;
+
+let sumQandW = q + w;
+console.log("sum =", sumQandW);
+
+let e = -10;
+console.log(-e);
+
+// Задание 6
+
+let myName = {
+    firstName: "Artem",
+    lastName: "Nordakov",
+};
+console.log(`имя: ${myName.firstName} фамилия: ${myName.lastName}`);
+
+const java = "java";
+const script = "script";
+console.log(java + script);
+
+const hi = "hello";
+const mir = "world";
+console.log(hi + " " + mir);
