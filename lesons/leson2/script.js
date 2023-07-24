5dc924e57ef5bc6ef782723bd454511d3ac71e6c
Nord.A <mr.nordakov@gmail.com>
2023-07-24 15:27:31 +0300

    leson2

---
+++ lesons/leson2/script.js
@@ -0,0 +1,42 @@
+// alert("Hello");
+
+// prompt("Input you age: ", 18);
+
+// confirm("Продолжить работу на странице?");
+
+
+
+// let userAnswer = prompt("Зимой и летом одним цветом: ")
+
+// if (userAnswer === "") {
+//     alert("Пустое значение")
+//     console.log("пусто");
+// } else if (userAnswer === "елка"){
+//     alert("верно");
+//     console.log("верно");
+// } else {
+//     alert("не верно");
+//     console.log("не верно");
+// }
+
+// Преобразование:
+
+// let pass = prompt('Введите пароль:'); // '123'
+// pass = Number(pass); // 123
+// // pass = String(pass); // '123'
+
+// if (pass === 123) {
+//     alert('Go');    
+// } else {
+//     alert('Stop');
+// }
+
+let pass = Number(prompt('Введите пароль:'));
+alert((pass === 123) ? 'Проходим дальше' : 'Проход запрещен');
+
+// (условие) ? ДА : НЕТ
+
+// console.log(userAnswer);
+
+// (userAnswer === "Елка") ? alert("верно") : alert("не верно");
+
