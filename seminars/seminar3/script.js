2c6961b74c939db2b4bcc05dd738f127d54d3a80
Nord.A <mr.nordakov@gmail.com>
2023-07-28 13:01:05 +0300

    seminar3.1

--- seminars/seminar3/script.js
+++ seminars/seminar3/script.js
@@ -21,13 +21,12 @@
 
 // console.log(arrChar);
 
-
 // task 2.1
 
 // const arrNumbers = [1, 2, 3];
 
 // for (let i = 0; i < arrNumbers.length; i++) {
-//     arrNumbers[i]++;  
+//     arrNumbers[i]++;
 // }
 
 // console.log(arrNumbers);
@@ -41,14 +40,14 @@
 // console.log(arr);
 // console.log(`Длина массива: ${arr.length}`);
 
-//task 2.3 
+//task 2.3
 
 // const arr = [1, 2, 3];
 
 // arr.push(4, 5);  // 1 вариант
 
 // for (let i = 4; i <= 5; i++) {  // 2 вариант
-//     arr.push(i);  
+//     arr.push(i);
 // }
 
 // console.log(arr);
@@ -59,16 +58,15 @@
 // console.log(arrAny.length);
 
 // for (let i = 0; i < 2; i++) {
-//     arrAny.pop();  
+//     arrAny.pop();
 // }
 
 // console.log(arrAny.length);
 
-
 // task 3.1
 
 // for (let i = 11; i <= 33; i++) {
-//     console.log(i); 
+//     console.log(i);
 // }
 
 // task 3.2
@@ -87,16 +85,144 @@
 //     console.log(i);
 // }
 
-let num = 3;
-let count = 0;
+// task 3.4
 
-for (let i = 0; ; i++) {
-    if (num < 1000) {
-        num = num * 3;
-        count++;
+// let num = 3;
+// let count = 0;
+
+// while (num <= 1000) {  // вариант с while
+//     num *= 3;
+//     count++;
+// }
+
+// for (let i = 0; ; i++) {
+//   // вариант с for
+//   if (num < 1000) {
+//     num = num * 3;
+//     count++;
+//   } else {
+//     break;
+//   }
+// }
+
+// console.log(`Число итераций для получения ${num} = ${count}`);
+
+// // task 4.1
+
+// const arr = [2, 5, 9, 15, 1, 4];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 3 && arr[i] < 10) {
+//     console.log(arr[i]);
+//   } else {
+//     continue;
+//   }
+// }
+
+// // task 4.2
+
+// let sum = 0;
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     sum += i;
+//   } else {
+//     continue;
+//   }
+// }
+
+// console.log(`Сумма четных чисел от 2 до 100 = ${sum}.`);
+
+// // task 4.3
+
+// const arr = [2, 5, 9, 3, 1, 4];
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+
+// console.log(sum);
+
+// // task 4.4
+
+// const arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// console.log(arrNum.join("-"));
+
+// let stringNum = "";
+
+// for (let i = 1; i <= 10; i++) {
+//   stringNum += "-";
+//   if (i < 10) {
+//     stringNum += i;
+//   } else {
+//     break;
+//   }
+// }
+
+// console.log(stringNum);
+
+// // task 4.5
+
+// const arrNum = [2, 5, 9, 0, 3, 1, 4];
+
+// for (let i = 0; i < arrNum.length; i++) {
+//   if (arrNum[i] !== 0) {
+//     console.log(arrNum[i]);
+//   } else {
+//     break;
+//   }
+// }
+
+// // task 5.1
+
+// const arrAny = [2, 4, 1, 3, 1, 6, 8, 9, 0];
+
+// for (let i = 0; i < arrAny.length; i++) {
+//   if (arrAny[i] % 2 === 0) {
+//     console.log(Math.pow(arrAny[i], 2));
+//   } else if (arrAny[i] % 3 === 0) {
+//     console.log(Math.pow(arrAny[i], 3));
+//   }
+// }
+
+// task 5.2
+
+const stringArr1 = [];
+for (let i = 0; i < 2; i++) {
+  const stringArr2 = [];
+
+  for (let j = 1; j <= 3; j++) {
+    if (i === 1) {
+      stringArr2[j] = j + 3;
     } else {
-        break;
+      stringArr2[j] = j;
     }
+  }
+  stringArr1.push(stringArr2);
 }
 
-console.log(`Число итераций для получения ${num} = ${count}`);
+console.log(stringArr1);
+
+// // task 5.3
+
+// const arr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
+// let count = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 3) {
+//     count++;
+//   } else {
+//     continue;
+//   }
+// }
+
+// console.log(count);
+
+// // task 5.4
+
+// const arrNum = [1, 2, 3, 4, 5];
+
+// arrNum.splice(1, 2);
+// console.log(arrNum);
