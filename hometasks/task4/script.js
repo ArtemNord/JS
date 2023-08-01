0e7c8133fc0b16d54a555ec53f12fda998fc5d38
Nord.A <mr.nordakov@gmail.com>
2023-08-01 16:14:21 +0300

    task 4.1

---
+++ hometasks/task4/script.js
@@ -0,0 +1,50 @@
+// // Задание 1
+// // Дан объект numbers. Необходимо в консоль вывести все значения больше или равные 3.
+
+// const numbers = {
+//   keyin1: 1,
+//   keyin2: 2,
+//   keyin3: 3,
+//   keyin4: 4,
+//   keyin5: 5,
+//   keyin6: 6,
+//   keyin7: 7,
+
+//   moreThanThree() {
+//     for (const key in numbers) {
+//       if (numbers[key] >= 3) {
+//         console.log(numbers[key]);
+//       }
+//     }
+//   },
+// };
+
+// numbers.moreThanThree();
+
+// Задание 2
+// Необходимо из объекта, который лежит в константе post вывести значения, к которым приписан комментарий, в консоль.
+
+const post = {
+  author: "John", // вывести этот текст
+  postId: 23,
+  comments: [
+    {
+      userId: 10,
+      userName: "Alex",
+      text: "lorem ipsum",
+      rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+      },
+    },
+    {
+      userId: 5, // вывести это число
+      userName: "Jane",
+      text: "lorem ipsum 2", // вывести этот текст
+      rating: {
+        likes: 3,
+        dislikes: 1,
+      },
+    },
+  ],
+};
